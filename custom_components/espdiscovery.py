c9248d82787d750948c50491f1fa859f2ffdcb1e
Giovanni Tarter <giovanni.tarter@gmail.com>
2016-12-26 11:56:36 +0000

    hiding heater switch, google calendars, etc

--- custom_components/espdiscovery.py
+++ custom_components/espdiscovery.py
@@ -15,16 +15,29 @@ def setup(hass, config):
         deviceid = hostname 
         devicename = deviceid[-4:].upper()
         
+        switchnr = "0"
+        switch_name = "sw{}_{}".format(
+            switchnr,
+            devicename,
+            )
+        entity_id = "switch.{}".format(switch_name)
         load_platform(hass, 'switch', "espthermostat", {
             "deviceid" : deviceid,
-            "switchnr" : "0",
-            "name" : devicename,
+            "name" : switch_name,
+            "switchnr" : switchnr,
+            "hide" : True,
             })
-        
+
+        switchnr = "1"
+        switch_name = "sw{}_{}".format(
+            switchnr,
+            devicename,
+            )
         load_platform(hass, 'switch', "espthermostat", {
             "deviceid" : deviceid,
-            "switchnr" : "1",
-            "name" : devicename,
+            "name" : switch_name,
+            "switchnr" : switchnr,
+            "hide" : False,
             })
         
         load_platform(hass, 'sensor', "espthermostat", {
@@ -32,13 +45,19 @@ def setup(hass, config):
             "type" : "humidity",
             "name" : devicename,
             })
-        
+        tempgrp.update_tracked_entity_ids(
+                ["sensor.hum_{}".format(devicename)])
+      
+
         load_platform(hass, 'sensor', "espthermostat", {
             "deviceid" : deviceid,
             "type" : "temperature",
             "name" : devicename,
             })
+        humgrp.update_tracked_entity_ids(
+                ["sensor.temp_{}".format(devicename)])
         
+
         load_platform(hass, 'climate', "espthermostat", {
             "name" : devicename,
             "deviceid" : deviceid,
@@ -48,9 +67,9 @@ def setup(hass, config):
     def message_received(topic, payload, qos):
         #"""A new MQTT message has been received."""
         #hass.states.set(entity_id, payload)
-        print("###############")
-        print(payload)
-        print("###############")
+        #print("###############")
+        #print(payload)
+        #print("###############")
         
         if payload not in discovered:
             discovered.append(payload)
@@ -60,11 +79,17 @@ def setup(hass, config):
 
     """Setup the component."""
     mqtt = loader.get_component('mqtt')
+    group = loader.get_component('group')
+    persistent_notification = loader.get_component('persistent_notification')
+
     #topic = config[DOMAIN].get('topic', DEFAULT_TOPIC)
     #entity_id = 'hello_mqtt.last_message'
     topic = "espdiscovery"
     entity_id = ""
 
+    #switches = group.Group.create_group(hass, "HeaterSW", view=True)
+    tempgrp = group.Group.create_group(hass, "Temperature", view=False)
+    humgrp = group.Group.create_group(hass, "Humidity", view=False)
     discovered = []
     mqtt.subscribe(hass, topic, message_received)
 
--- custom_components/switch/espthermostat.py
+++ custom_components/switch/espthermostat.py
@@ -1,37 +1,46 @@
 import homeassistant.components.switch.mqtt as mqsw
+from homeassistant.helpers.entity import Entity
+
+class HidableMQTTSW(mqsw.MqttSwitch):
+
+    def __init__(self, hass, name, state_topic, command_topic, qos, retain,
+            payload_on, payload_off, optimistic, value_template, hide):
+        super().__init__(hass, name, state_topic, command_topic, qos, retain,
+                payload_on, payload_off, optimistic, value_template)
+
+        self._hidden = hide
+
+    @property
+    def hidden(self) -> bool:
+        return self._hidden
+
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Your switch/light specific code."""
     
     deviceid = discovery_info["deviceid"]
+    hide = discovery_info["hide"]
     switchnr = discovery_info["switchnr"]
     friendlyname = discovery_info["name"]
 
-    switch_name = "sw{}_{}".format(
-            switchnr,
-            friendlyname,
-            )
+    mqttsw = HidableMQTTSW(
+                 hass,
+                 friendlyname,
+                 "devices/{}/switch{}/state".format(
+                     deviceid,
+                     switchnr),
+                 "devices/{}/switch{}/cmd".format(
+                     deviceid,
+                     switchnr),
+                 0,
+                 True,
+                 "ON",
+                 "OFF",
+                 False,
+                 None,
+                 hide,
+                 )
+    add_devices([mqttsw])
 
-    switch_name = switch_name.upper()
-    add_devices(
-                [
-                mqsw.MqttSwitch(
-                    hass,
-                    switch_name,
-                    "devices/{}/switch{}/state".format(
-                        deviceid,
-                        switchnr),
-                    "devices/{}/switch{}/cmd".format(
-                        deviceid,
-                        switchnr),
-                    0,
-                    True,
-                    "ON",
-                    "OFF",
-                    False,
-                    None
-                    ),
-                ]
-                )
+
 
