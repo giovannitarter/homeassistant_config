86d8b077604de95b99abe70e8ac7e79f6bf44399
Giovanni Tarter <giovanni.tarter@gmail.com>
2017-10-01 06:43:15 +0200

    fixes

--- custom_components/switch/espthermostat.py
+++ custom_components/switch/espthermostat.py
@@ -3,10 +3,13 @@ from homeassistant.helpers.entity import Entity
 
 class HidableMQTTSW(mqsw.MqttSwitch):
 
-    def __init__(self, hass, name, state_topic, command_topic, qos, retain,
-            payload_on, payload_off, optimistic, value_template, hide):
-        super().__init__(name, state_topic, command_topic, qos, retain,
-                payload_on, payload_off, optimistic, value_template)
+    def __init__(self, hass, name, state_topic, command_topic, availability_topic,
+                    qos, retain, payload_on, payload_off, optimistic,
+                    payload_available, payload_not_available, value_template, hide):
+    
+        super().__init__(name, state_topic, command_topic, availability_topic,
+                    qos, retain, payload_on, payload_off, optimistic,
+                    payload_available, payload_not_available, value_template)
 
         self._hidden = hide
 
@@ -24,21 +27,26 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     friendlyname = discovery_info["name"]
 
     mqttsw = HidableMQTTSW(
-                 hass,
-                 friendlyname,
-                 "devices/{}/switch{}/state".format(
+                 hass=hass,
+                 name=friendlyname,
+                 state_topic="devices/{}/switch{}/state".format(
                      deviceid,
                      switchnr),
-                 "devices/{}/switch{}/cmd".format(
+                 command_topic="devices/{}/switch{}/cmd".format(
                      deviceid,
                      switchnr),
-                 0,
-                 True,
-                 "ON",
-                 "OFF",
-                 False,
-                 None,
-                 hide,
+                 availability_topic="devices/{}/switch{}/avail".format(
+                     deviceid,
+                     switchnr),
+                 qos=0,
+                 retain=True,
+                 payload_on="ON",
+                 payload_off="OFF",
+                 optimistic=False,
+                 payload_available="ON",
+                 payload_not_available="OFF",
+                 value_template=None,
+                 hide=hide,
                  )
     add_devices([mqttsw])
 
